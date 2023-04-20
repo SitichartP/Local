00b399b13911bd13a75543520dec85bd2ff2b1ab
SitichartP <124141690+SitichartP@users.noreply.github.com>
2023-04-20 11:35:26 +0700

    Add files via upload

--- oss/oss_script.py
+++ oss/oss_script.py
@@ -10,10 +10,12 @@ st.set_page_config(page_title="Oss Task", page_icon=None, layout="wide", initial
 df = pd.read_excel("OSS Concept Design Delivery Guide.xlsx",index_col=False,sheet_name="Sheet1")
 df.drop(df.columns[[0,1]],axis=1,inplace=True)
 
-
 ## Create list of df for each seg ## 
 df_pm = df.loc[(df["PM "] == "X") & (df["Electrical Team"] != "X") & (df["Civl Structural Team"] != "X")]
 df_pm.drop(["Civl Structural Team","Electrical Team"],axis = 1 , inplace=True)
+
+df_pm_all = df_pm.loc[df_pm["Classification"] == "Mandatory"]
+
 
 ## Create Optional Task ## 
 df_pm_op = df_pm.loc[df_pm["Classification"] == "Optional"]
@@ -25,12 +27,11 @@ selected_pm = st.sidebar.selectbox('Select a optional task', ['All'] + list(df_p
 ## Task Project Management ##
 st.title("Project Management")
 
-selected_pm_fin = df_pm[df_pm["Artefact"] == selected_pm]
-
-if selected_pm == ["All"]:
-    st.table(df_pm)
+if selected_pm == "All":
+    st.table(df_pm_all)
 else: 
-    st.table(df_pm)
+    df_concat = pd.concat([df_pm_all,df_pm.loc[df_pm["Artefact"] == selected_pm]])
+    st.table(df_concat)
 
 
 
