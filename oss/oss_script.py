88b31d0abe3f2b6c9bddcc403b25b76b8d93af8b
jay-siv18 <pochara.sivasirikarul@mottmac.com>
2023-04-20 13:20:34 +0700

    Update oss_script.py
    variable names

--- oss/oss_script.py
+++ oss/oss_script.py
@@ -14,41 +14,62 @@ if uploaded_file is not None:
     # df.drop(df.columns[[0,1]],axis=1,inplace=True)
 
 
-    ## Create list of df for each seg ## 
+    ######################################## DATAFRAMES FOR EACH SECTION ######################################## 
     df_pm = df.loc[(df["PM "] == "X") & (df["Electrical Team"] != "X") & (df["Civl Structural Team"] != "X")]
     df_pm.drop(["Civl Structural Team","Electrical Team"],axis = 1 , inplace=True)
-
-    df_pm_all = df_pm.loc[df_pm["Classification"] == "Mandatory"]
-
-    # Dataframe to sum hours for PM
-    pm_sum_df = pd.DataFrame 
-
-    ## Create Optional Task ## 
+    df_pm_man = df_pm.loc[df_pm["Classification"] == "Mandatory"]
     df_pm_op = df_pm.loc[df_pm["Classification"] == "Optional"]
+    
+    df_elec =
+    df_elec.drop 
+    df_elec_man = 
+    df_elec_op = 
 
-    ## Create Side bar filter ## 
+    df_civ = 
+    df_civ.drop 
+    df_civ_man = 
+    df_civ_op = 
+
+    df_ec = 
+    df_ec.drop 
+    df_ec_man = 
+    df_ec_op = 
+
+    df_pec =
+    df_pec.drop
+    df_pec_man = 
+    df_ec_op = 
+
+
+
+    ######################################## SIDE BAR FILERS ######################################## 
     st.sidebar.header('PM Optional Tasks')
     selected_pm = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_pm_op['Artefact'].unique()))
+    # selected_elec =
+    # selected_civ =
+    # selected_ec = 
+    # selected_pec = 
 
-    ## Task Project Management ##
+    ## TITLE ##
     st.title("Delivery Schedule")
+
+    ## PROJECT MANAGEMENT SECTION ##
     st.subheader("Project Management")
 
     if selected_pm == "All":
-        st.table(df_pm_all)
-        # pm_sum_df['Total Estimated Labour Hours'] = [df_pm_all['Est Labour Hours'].sum()]
-        # pm_sum_df['Total Timeline'] = [df_pm_all['Timeline'].sum()]
-        st.markdown(f"Total Estimated Labour Hours: **{df_pm_all['Est Labour Hours'].sum()}**")
-        st.markdown(f"Total Timeline: **{df_pm_all['Timeline'].sum()}**")
+        st.table(df_pm_man)
+        st.markdown(f"Total Estimated Labour Hours: **{df_pm_man['Est Labour Hours'].sum()}**")
+        st.markdown(f"Total Timeline: **{df_pm_man['Timeline'].sum()}**")
         
     else: 
-        df_concat = pd.concat([df_pm_all,df_pm.loc[df_pm["Artefact"] == selected_pm]])
+        df_concat = pd.concat([df_pm_man,df_pm.loc[df_pm["Artefact"] == selected_pm]])
         st.table(df_concat)
-        # pm_sum_df['Total Estimated Labour Hours'] = [df_concat['Est Labour Hours'].sum()]
-        # pm_sum_df['Total Timeline'] = [df_concat['Timeline'].sum()]
         st.markdown(f"Total Estimated Labour Hours: **{df_concat['Est Labour Hours'].sum()}**")
         st.markdown(f"Total Timeline: **{df_concat['Timeline'].sum()}**")
 
+    st.subheader("Project Management")
+
+    
 
 ## Electrical ## 
 
