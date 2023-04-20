519a62a77fea2498f97c4923b3db0c61fcb115fa
Sitichart Pakpab <Sitichart.Pakpab@mottmac.com>
2023-04-20 13:43:06 +0700

    Update oss_script.py
    Add Sidebar variables.

--- oss/oss_script.py
+++ oss/oss_script.py
@@ -2,7 +2,12 @@ import pandas as pd
 import numpy  as np
 import streamlit as st 
 
+## PAGE CONFIG ## 
+
 st.set_page_config(page_title="Oss Task", page_icon=None, layout="wide", initial_sidebar_state="auto")
+
+## TITLE ##
+st.title("Delivery Schedule")
 
 ## Import dataframe in each section ## 
 
@@ -20,38 +25,48 @@ if uploaded_file is not None:
     df_pm_man = df_pm.loc[df_pm["Classification"] == "Mandatory"]
     df_pm_op = df_pm.loc[df_pm["Classification"] == "Optional"]
     
-    # df_elec =
-    # df_elec.drop 
-    # df_elec_man = 
-    # df_elec_op = 
+    df_elec = df.loc[(df["PM "] != "X") & (df["Electrical Team"] == "X") & (df["Civl Structural Team"] != "X")]
+    df_elec.drop(["Civl Structural Team","PM "],axis = 1 , inplace=True)
+    df_elec_man = df_elec.loc[df_elec["Classification"] == "Mandatory"]
+    df_elec_op = df_elec.loc[df_elec["Classification"] == "Optional"]
 
-    # df_civ = 
-    # df_civ.drop 
-    # df_civ_man = 
-    # df_civ_op = 
+    df_civ = df.loc[(df["PM "] != "X") & (df["Electrical Team"] != "X") & (df["Civl Structural Team"] == "X")]
+    df_civ.drop(["Electrical Team","PM "],axis = 1 , inplace=True)
+    df_civ_man = df_civ.loc[df_civ["Classification"] == "Mandatory"]
+    df_civ_op = df_civ.loc[df_civ["Classification"] == "Optional"]
 
-    # df_ec = 
-    # df_ec.drop 
-    # df_ec_man = 
-    # df_ec_op = 
+    df_ec = df.loc[(df["PM "] == "X") & (df["Electrical Team"] == "X") & (df["Civl Structural Team"] != "X")]
+    df_ec.drop(["Civl Structural Team","PM "],axis = 1 , inplace=True) 
+    df_ec_man = df_ec.loc[df_ec["Classification"] == "Mandatory"]
+    df_ec_op = df_ec.loc[df_ec["Classification"] == "Optional"]
 
-    # df_pec =
-    # df_pec.drop
-    # df_pec_man = 
-    # df_pec_op = 
+    df_pec = df.loc[(df["PM "] == "X") & (df["Electrical Team"] == "X") & (df["Civl Structural Team"] == "X")]
+    df_pec_man = df_pec.loc[df_pec["Classification"] == "Mandatory"]
+    df_pec_op = df_pec.loc[df_ec["Classification"] == "Optional"]
 
 
 
     ######################################## SIDE BAR FILERS ######################################## 
+
+    ## PM ##
     st.sidebar.header('PM Optional Tasks')
     selected_pm = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_pm_op['Artefact'].unique()))
-    # selected_elec =
-    # selected_civ =
-    # selected_ec = 
-    # selected_pec = 
 
-    ## TITLE ##
-    st.title("Delivery Schedule")
+    ## Electrical ##
+    st.sidebar.header('Electrical Optional Tasks')
+    selected_elec = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_elec_op['Artefact'].unique()))   
+
+    ## Civil ##
+    st.sidebar.header('Civil and Structural Optional Tasks')
+    selected_civ = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_civ_op['Artefact'].unique()))   
+
+    ## Electrical and civil structural ##
+    st.sidebar.header('Electrical and civil structural Tasks')
+    selected_ec = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_ec_op['Artefact'].unique())) 
+
+    ## Prequalification of OEMs, FEED Consultants and EPC(I) contractors & Tender support (Electrical and civil structural) ## 
+    st.sidebar.header('OEMs,FEED Consultants and EPC(I) Tasks')
+    selected_pec = st.sidebar.selectbox('Select a optional task', ['Please Select'] + list(df_pec_op['Artefact'].unique())) 
 
     ##### PROJECT MANAGEMENT SECTION #####
     st.subheader("Project Management")
@@ -84,7 +99,7 @@ if uploaded_file is not None:
     st.subheader("Budget Estimation of OSS Costs")
 
     if selected_ec:
-        df_concat_civ = pd.concat([df_ec_man,df_ec.loc[df_ec["Artefact"] == selected_ec]])
+        df_concat_ec = pd.concat([df_ec_man,df_ec.loc[df_ec["Artefact"] == selected_ec]])
         st.table(df_concat_ec)
         st.markdown(f"Total Estimated Labour Hours: **{df_ec['Est Labour Hours'].sum()}**")
         st.markdown(f"Total Timeline: **{df_ec['Timeline'].sum()}**")
@@ -93,7 +108,7 @@ if uploaded_file is not None:
     st.subheader("Prequalifications of OEMS, FEED Consultants and EPC(I) contractors & Tender support")
 
     if selected_pec:
-        df_concat_civ = pd.concat([df_pec_man,df_pec.loc[df_pec["Artefact"] == selected_pec]])
+        df_concat_pec = pd.concat([df_pec_man,df_pec.loc[df_pec["Artefact"] == selected_pec]])
         st.table(df_concat_pec)
         st.markdown(f"Total Estimated Labour Hours: **{df_pec['Est Labour Hours'].sum()}**")
         st.markdown(f"Total Timeline: **{df_pec['Timeline'].sum()}**")
